2a74deaefeb41bfb89adf4e4adc305afb344f295
Jackson Empey <47725944+15jgme@users.noreply.github.com>
2023-02-12 21:21:54 +0000

    Fix bugs, push Authors list to db

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import arxiv from 'arxiv-api';
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('http://149.248.62.135:15007');
+const pb = new PocketBase('https://moonflowerdata.jgme.io');
 let lastPaperId = '';
 
 //Do an initial check on how many articles we have
@@ -13,6 +13,7 @@ if (resultList.totalItems > 0) {
   let lastPaper = resultList.items.pop(); // Assumes we dont need result list in the future
   resultList = []; // Breaking to remember the above change
   lastPaperId = lastPaper['entry_id'];
+  console.log(lastPaperId)
 }
 
 async function fetchPaper() {
@@ -38,26 +39,24 @@ async function fetchPaper() {
   ) {
     console.log("Checking papers at a depth of " + currentSearchLen)
     // See if our limit is too high
-    if (currentSearchLen > 10) {
+    if (currentSearchLen > 100) {
       limitReached = true;
     } else {
-      papers = await arxiv.search({
+      papers = (await arxiv.search({
         searchQueryParams: searhParamList,
         start: 0,
         maxResults: currentSearchLen,
         sortBy: "submittedDate",
         sortOrder: "descending"
-      });
-      if (lastPaperId == '') { limitReached = true; }
-      else {
-        papers.forEach((paper, i) => {
-          if (paper['id'] == lastPaperId) {
-            lastPaperId = paper['id'];
-            lastPaperFound = true;
-            index_of_last_paper = i;
-          }
-        })
-      }
+      })).reverse(); // reverse order to PB and arxiv match
+
+      papers.forEach((paper, i) => {
+        if (paper['id'] == lastPaperId) {
+          lastPaperFound = true;
+          index_of_last_paper = i;
+        }
+      })
+
       // Check if we didn't find anything
       // And increase search depth by 20
       if (!lastPaperFound) { currentSearchLen += 20; }
@@ -65,55 +64,54 @@ async function fetchPaper() {
   }
 
   let i = 0;
+  console.log(papers.length)
   console.log(lastPaperId);
   console.log(index_of_last_paper);
   console.log(lastPaperFound);
-  if (index_of_last_paper > 0) {
-    for await (const paper of papers) {
-      const paper = papers[i];
 
-      // Check if the paper we're looking at is new 
-      if ((i < index_of_last_paper) || (!lastPaperFound)) {
-        console.log("Uploading paper " + i + " current id " + paper['id'])
+  for await (const paper of papers) {
 
-        // Get catagory ids for this paper
-        let catIds = []
-        paper['categories'].forEach(catagory => {
-          catagory = catagory.term;
-          // for each catagory in the paper, 
-          // if we have it in the ID map, push it to our ID map array
-          if (catIdMap.hasOwnProperty(catagory)) {
-            catIds.push(catIdMap[catagory]);
-          }
+    // Check if the paper we're looking at is new 
+    if ((i < index_of_last_paper) || (!lastPaperFound)) {
+      console.log("Uploading paper " + i + " current id " + paper['id'])
 
-        });
-        // Assemble data
-        const paperLinks = paper['links'];
-        const pdf_link = paperLinks[1].href
-        console.log(pdf_link);
-        const data = {
-          "entry_id": paper['id'],
-          "title": paper['title'],
-          "created": paper['published'],
-          "summary": paper['summary'],
-          "doi": paper['doi'],
-          // "primary_catagory": "RELATION_RECORD_ID",
-          "catagories": catIds,
-          "pdf_url": pdf_link
-        };
-        console.log(reachedB)
-        try {
-          await pb.collection('articles').create(data, { '$autoCancel': false }); // Push new paper data
-        } catch (error) {
-          console.log("The following error occured" + error);
-        } finally {
-          console.log("Something went wrong with the article upload");
+      // Get catagory ids for this paper
+      let catIds = []
+      paper['categories'].forEach(catagory => {
+        catagory = catagory.term;
+        // for each catagory in the paper, 
+        // if we have it in the ID map, push it to our ID map array
+        if (catIdMap.hasOwnProperty(catagory)) {
+          catIds.push(catIdMap[catagory]);
         }
 
+      });
+      // Assemble data
+      const paperLinks = paper['links'];
+      const pdf_link = paperLinks[1].href
+      console.log(pdf_link);
+      const data = {
+        "entry_id": paper['id'],
+        "title": paper['title'],
+        "created": paper['published'],
+        "summary": paper['summary'],
+        "authors": JSON.stringify(paper['authors']),
+        "doi": paper['doi'],
+        // "primary_catagory": "RELAfTION_RECORD_ID",
+        "catagories": catIds,
+        "pdf_url": pdf_link
+      };
+
+      try {
+        await pb.collection('articles').create(data, { '$autoCancel': false }); // Push new paper data
+      } catch (error) {
+        console.log("The following error occured" + error);
       }
-      i = i + 1;
+
     }
+    i = i + 1;
   }
+
 
   console.log("Finished fetch")
 }
